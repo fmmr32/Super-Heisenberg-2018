b5404daf246fbc68cd18c169d372cd48e7a34ba0
guitargun <stephen@tollenaar.com>
2018-01-10 15:00:07 -0330

    seperated the sprite creation.. added a new type of tile

--- JavaScriptTest/js/map.js
+++ JavaScriptTest/js/map.js
@@ -1,84 +1,18 @@
 ﻿
-var tiles;
-
 MAP = function (container) {
     //make function that loads a resource from somewhere containing info of below
 
     var temp = new Map();
-    var floor = '[ {"name": "dirt", "source": "../JavaScriptTest/resources/tiles.png", "width": 32,"heigth": 10,"startX": 0,"startY": 12}, {"name":"stone","source": "../JavaScriptTest/resources/tiles.png", "width": 32,"heigth": 10,"startX": 0,"startY": 12}]';
-    floor = JSON.parse(floor);
-   
+    //need something better fort this
+    var floor = '[ {"id":1, "name": "dirt", "source": "../JavaScriptTest/resources/tiles.png", "width": 32,"heigth": 10,"startX": 0,"startY": 12}, {"id":2,"name":"stone","source": "../JavaScriptTest/resources/tiles.png", "width": 32,"heigth": 10,"startX": 0,"startY": 43}]';
 
-    for (data of floor) {
-        var options = {};
-        options.context = container.getContext("2d");
-        options.image = {};
+    loadSprites(floor, container);
 
-        //from tiles.png if it is a tile (floor), from character.png if character, object.png if trap etc...
-        options.image.src = data.source;
-
-
-        //these values differ from sprite to sprite
-        options.width = data.width;
-        options.heigth = data.heigth;
-        options.image.startX = data.startX;
-        options.image.startY = data.startY;
-
-        //mapping it
-        temp.set(data.name, options);
-    }
-
-    
-    tiles = loadTiles(temp);
-    console.log(tiles);
-
+    getSprite(2).draw(150, 100);
+    getSprite(1).draw(100, 100); 
 };
 
-//simeple sprite loader
-function loadTiles(tiles) {
-    var temp = new Map();
-    for (spriteName of tiles.keys()) {
-        temp.set(spriteName, new Sprite(tiles.get(spriteName)));
-    }
-    return temp;
-}
 
 
-
-
-function Sprite(options) {
-    this.context = options.context;
-    this.width = options.width;
-    this.heigth = options.heigth;
-    this.image = options.image;
-
-}
-
-Sprite.prototype = {
-    //draw function for the sprite
-    draw: function (desX, desY) {
-        //all the values that the sprite holds
-        var img = new Image();
-        var width = this.width;
-        var heigth = this.heigth;
-        var context = this.context;
-        var image = this.image;
-        //draws the actual image when the image is loaded
-        img.onload = function () {
-            context.drawImage(
-                img, //the image
-                image.startX, //start x of the image in the spritesheet
-                image.startY, //start y of the image in the spritesheet
-                width, //width of the sprite
-                heigth, //height of the sprite
-                desX, //destination x of the sprite on the canvas
-                desY, //destination of y of the sprite on the canvas
-                width, //width of sprite
-                heigth //heigth of sprite
-            );
-        };
-        img.src = image.src;
-    }
-};
 
 
---
+++ JavaScriptTest/js/sprite.js
@@ -0,0 +1,68 @@
+﻿var sprites = new Map();
+
+SPRITE = function (options) {
+    this.context = options.context;
+    this.width = options.width;
+    this.heigth = options.heigth;
+    this.image = options.image;
+    this.name = options.name;
+    this.id = options.id;
+};
+
+
+SPRITE.prototype = {
+    //draw function for the sprite
+    draw: function (desX, desY) {
+        //all the values that the sprite holds
+        var img = new Image();
+        var width = this.width;
+        var heigth = this.heigth;
+        var context = this.context;
+        var image = this.image;
+        //draws the actual image when the image is loaded
+        img.onload = function () {
+            context.drawImage(
+                img, //the image
+                image.startX, //start x of the image in the spritesheet
+                image.startY, //start y of the image in the spritesheet
+                width, //width of the sprite
+                heigth, //height of the sprite
+                desX, //destination x of the sprite on the canvas
+                desY, //destination of y of the sprite on the canvas
+                width, //width of sprite
+                heigth //heigth of sprite
+            );
+        };
+        img.src = image.src;
+    }
+};
+
+function newSprite(options) {
+    var sprite = new SPRITE(options);
+    sprites.set(options.id, sprite);
+}
+
+function getSprite(name) {
+    return sprites.get(name);
+}
+
+function loadSprites(json, container) {
+    var any = JSON.parse(json);
+
+    for (var tile of any) {
+        var options = {};
+        options.context = container.getContext("2d");
+        options.image = {};
+
+        //from tiles.png if it is a tile (floor), from character.png if character, object.png if trap etc...
+        options.image.src = tile.source;
+        options.id = tile.id;
+        options.name = tile.name;
+        //these values differ from sprite to sprite
+        options.width = tile.width;
+        options.heigth = tile.heigth;
+        options.image.startX = tile.startX;
+        options.image.startY = tile.startY;
+        newSprite(options);
+    }
+}
