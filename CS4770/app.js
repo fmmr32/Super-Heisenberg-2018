6af72d56269147e7aff4142fcd2f5531adf7794d
guitargun <stephen@tollenaar.com>
2018-01-22 15:12:55 -0330

    edited some things

---
+++ CS4770/app.js
@@ -0,0 +1,35 @@
+﻿var mongojs = require('mongojs');
+var express = require('express');
+var fs = require('fs');
+var app = express();
+var serv = require('http').Server(app);
+
+app.use(express.static(__dirname + '/client'));
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+
+
+
+serv.listen(3000);
+console.log("Server started.");
+
+var io = require('socket.io')(serv, {});
+io.sockets.on('connection', function (socket) {
+    console.log('socket connection');
+
+   
+    socket.on('loadJSON', function (data) {
+        console.log(data.fileName);
+        var fileName = data.fileName;
+        fs.readFile(__dirname + data.fileName, "utf8", function read(err, data) {
+            if (err) {
+                throw err;
+            }
+
+            var substring = data.trim();
+
+            socket.emit(fileName, substring);
+        });
+    });
+}); 
