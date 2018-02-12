52d3ec75a974ac650f9a10b2731cf6f455a4a454
guitargun <Stephen@tollenaar.com>
2018-02-12 10:18:12 -0330

    added bat file for easy start up

---
+++ serverStart.bat
@@ -0,0 +1,5 @@
+start cmd /k  "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe"
+TIMEOUT 10
+start cmd /k "C:\Program Files\MongoDB\Server\3.6\bin\Mongo"
+TIMEOUT 10
+start cmd /k "CD C:\Users\Stephen\Source\Repos\CS4770\CS4770 & node app.js"
