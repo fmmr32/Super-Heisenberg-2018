5053668644756b17e29a9eecdbcb4c933f1ca4d5
BBFifield <bb.fifield@gmail.com>
2018-03-28 19:24:45 -0230

    Level browser doesn't duplicate levels inside the list and unsuccessful user login now doesn't go to the main menu

--- CS4770/client/js/server.js
+++ CS4770/client/js/server.js
@@ -2,26 +2,30 @@
 
 //This loads things from the database
 var loadDB = function (name, callback) {
-    console.log("Inside first");
     socket.emit('loadDB', {
         collection: name
     });
 
-    socket.on(name, function (data) {
-        callback(data);
-    });
+    if (!socket.hasListeners(name)) {
+        socket.on(name, function (data) {
+            callback(data);
+        });
+    }
+
+    
 }
 
 var loadDBFromID = function (name, id_object, callback) {
-    console.log("inside second");
     socket.emit('loadDBbasedID', {
         collection: name,
         id: id_object
     });
 
-    socket.on(name, function (data) {
-        callback(data);
-    });
+    if (!socket.hasListeners(name)) {
+        socket.on(name, function (data) {
+            callback(data);
+        });
+    }
 }
 
 var writeDB = function (name, object) {
@@ -49,41 +53,53 @@ var getUsername = function () {
     return playerUsername;
 }
 
+
 signDivSignIn.onclick = function () {
     playerUsername = signDivUsername.value;
     console.log(playerUsername);
     socket.emit('signIn', { username: signDivUsername.value, password: signDivPassword.value });
-    document.getElementById("signDiv").style.display = "none";
-    document.getElementById("mainMenu").style.display = "table-cell";
+
+    if (!socket.hasListeners("signInResponse")) {
+        socket.on('signInResponse', function (data) {
+            if (data.success) {
+                playerUsername = signDivUsername.value;
+                
+                document.getElementById("signDiv").style.display = "none";
+                document.getElementById("mainMenu").style.display = "table-cell";
+
+                loadMenu();
+            } else
+                alert("Sign in unsuccessul.");
+        });
+    }
 }
+
 
 signDivSignUp.onclick = function () {
     socket.emit('signUp', { username: signDivUsername.value, password: signDivPassword.value });
-    document.getElementById("signDiv").style.display = "none";
-    document.getElementById("mainMenu").style.display = "table-cell";
+
+    if (!socket.hasListeners("signUpResponse")) {
+        console.log("Inside sign up");
+        socket.on('signUpResponse', function (data) {
+            if (data.success) {
+                alert("Sign up successul.");
+
+                document.getElementById("signDiv").style.display = "none";
+                document.getElementById("mainMenu").style.display = "table-cell";
+            } else
+                alert("Sign up unsuccessul.");
+        });
+    }
 }
 
-socket.on('signInResponse', function (data) {
-    if (data.success) {
-        playerUsername = signDivUsername.value;
-        console.log(playerUsername);
-        loadMenu();
-    } else
-        alert("Sign in unsuccessul.");
-});
-
-socket.on('signUpResponse', function (data) {
-    if (data.success) {
-        alert("Sign up successul.");
-    } else
-        alert("Sign up unsuccessul.");
-});
 
 function loadJSONFile(callback, file) {
     socket.emit('loadJSON', {
         fileName: file
     });
-    socket.on(file, function (data) {
-        callback(data.toString());
-    });
+    if (!socket.hasListeners(name)) {
+        socket.on(file, function (data) {
+            callback(data.toString());
+        });
+    }
 }
