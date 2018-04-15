433ade3a2560f3a71d95448bfc4c7375f070f3c2
guitargun <stephen@tollenaar.com>
2018-04-15 20:06:08 -0230

    last time I worked on the damn aiming...

--- CS4770/client/js/overworld.js
+++ CS4770/client/js/overworld.js
@@ -217,6 +217,7 @@ class OverWorld {
         this.onOverWorld = true;
 
         this.isTestLevel = false;
+        this.isLevelBrowser = false;
         this.inShop = false;
         this.inCharacterSelect = false;
         this.inMuseum = false;
@@ -476,6 +477,7 @@ directions:
         //loading the map the player chose
         this.onOverWorld = false;
         this.music.stop();
+        this.isLevelBrowser = true;
         loaded = false;
         canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
         canvas.remove();
@@ -485,11 +487,12 @@ directions:
 
     //going back to the overworld
     toOverWorld(type) {
-        console.log(this.isTestLevel);
-        if (this.isTestLevel) {
+        if (this.isTestLevel || this.isLevelBrowser) {
             back();
             return;
         }
+        
+
 
         overWorld.inShop = false;
         this.inCharacterSelect = false;
